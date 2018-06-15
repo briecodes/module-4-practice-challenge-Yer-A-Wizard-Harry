ca0d32542808e41f63a5af6d514f644649720f1a
briecodes <hello@brie.work>
2018-06-14 22:42:12 -0400

    key for iteration fixed.

--- src/WizardList.js
+++ src/WizardList.js
@@ -3,14 +3,14 @@ import Wizard from "./Wizard";
 
 const WizardList = props => {
 
-  const students = props.students.map(student => {
-    return <Wizard name={student.name} house={student.house} />
+  const students = props.students.map((student, index) => {
+    return <Wizard key={index} name={student.name} house={student.house} />
   });
 
   const studentsFiltered = props.students.filter(student => {
     return student.house === props.currentHouse
-  }).map(student => {
-    return <Wizard name={student.name} house={student.house} />
+  }).map((student, index) => {
+    return <Wizard key={index} name={student.name} house={student.house} />
   });
 
   return <div className="WizardList">{ props.currentHouse === 'All' ? students : studentsFiltered }</div>;
